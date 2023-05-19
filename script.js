2fc21abba94451c56330f51b413397cbf5a1a506
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-05-19 17:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 let color = document.querySelector('#colorSelect');
-let selectedOptions = Array.from(color.selectedOptions);
+
 
     
 document.querySelector('input').addEventListener('click',()=>{
   // Remove the selected options
+	let selectedOptions = Array.from(color.selectedOptions);
   selectedOptions.forEach(function(option) {
     color.remove(option.index);
   });
