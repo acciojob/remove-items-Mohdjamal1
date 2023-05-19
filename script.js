b759a6c262a923847296b033a4b5601879496c0b
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-05-19 11:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let color = document.querySelector('#colorSelect');
+let selectedColor="";
+color.onchange = (ev) =>{
+  const index = color.selectedIndex;
+  selectedColor = color.options[index];
+}
+    
+document.querySelector('input').addEventListener('click',()=>{
+ selectedColor.remove();
+});
